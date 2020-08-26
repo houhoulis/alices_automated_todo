b6cf8cd451fb9bf9a8e22d363f82d5b778325c11
Chris Houhoulis <chris@chrishouhoulis.com>
2020-08-26 13:59:23 -0400

    Alice Goldfuss's automated todo system w/ tweaks.
    - Port to Ruby.
    - Use GitHub's preferred authentication: pass token via HTTP header.
    
    See https://blog.alicegoldfuss.com/automating-my-todo/

---
+++ weekly_release.rb
@@ -0,0 +1,86 @@
+require 'net/http'
+require 'open-uri' # to call URI.open (why is this needed, even after requiring net/http?)
+require 'json'
+require 'twilio-ruby'
+
+GITHUB_TOKEN = ENV['GITHUB_TOKEN'] # Personal access token from https://github.com/settings/tokens
+TWILIO_SID = ENV['TWILIO_ACCOUNT_SID'] # Account SID from twilio.com/console
+TWILIO_TOKEN = ENV['TWILIO_AUTH_TOKEN'] # Auth Token from twilio.com/console
+TWILIO_PHONE = ENV['MY_TWILIO_PHONE'] # Twilio account phone number
+MY_PHONE = ENV['MY_PHONE']
+
+HEADERS = {
+  'Accept' => 'application/vnd.github.inertia-preview+json',
+  'Authorization' => "token #{GITHUB_TOKEN}"
+}
+# Previously-retrieved column ids for the GitHub Project
+DONE    = '10567372'
+RELEASE = '10567392'
+
+def log filename, message
+  File.write (File.join ENV['TODO_PATH'], 'log', filename), message
+end
+
+def done_cards
+  return @_cards if @_cards
+  url = 'https://api.github.com/projects/columns/%s/cards' % DONE
+  @_cards = JSON.parse URI.open(url, HEADERS).read
+end
+
+# Collect the "Done" cards' notes
+def release_title
+  done_cards.map { |card| '- ' + card['note'] }.join "\n"
+end
+
+# Create new "Release" card using release_title
+def create_release_card
+  url = 'https://api.github.com/projects/columns/%s/cards' % RELEASE
+  uri = URI url
+  post = Net::HTTP::Post.new uri, HEADERS
+  post.body = { 'note' => release_title }.to_json
+  http = Net::HTTP.new uri.host, uri.port
+  http.use_ssl = 'https' == uri.scheme
+  response = http.request post
+  log 'release.log', "#{response.inspect}: #{response.body}: #{response.uri}" if response.code >= '400'
+end
+
+def send_text_with message
+  begin
+    client = Twilio::REST::Client.new TWILIO_SID, TWILIO_TOKEN
+    client.messages.create(
+      to: MY_PHONE,
+      from: TWILIO_PHONE,
+      body: "💪🏼Weekly Release! 🎉\n\n" + message
+    )
+  rescue StandardError => e
+    log 'twilio.log', e.exception.inspect
+    raise e
+  end
+end
+
+# Archive the "Done" cards
+def archive_done_cards
+  done_cards.each do |card|
+    card_id = card['id']
+    url = 'https://api.github.com/projects/columns/cards/%s' % card_id
+    uri = URI url
+    patch = Net::HTTP::Patch.new uri, HEADERS
+    patch.body = { 'archived' => true }.to_json
+    http = Net::HTTP.new uri.host, uri.port
+    http.use_ssl = 'https' == uri.scheme
+    response = http.request patch
+    log 'archive.log', "#{response.inspect}: #{response.body}: #{response.uri}" if response.code >= '400'
+  end
+end
+
+def release
+  create_release_card
+  send_text_with release_title
+  archive_done_cards
+rescue Exception => e
+  log 'error.log', e.exception.inspect
+  raise e
+end
+
+
+release
